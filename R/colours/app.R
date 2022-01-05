7996908370ee07f83b049e636ccd62ca05af5a27
lddurbin <l.d.durbin@gmail.com>
2022-01-05 18:43:38 +1300

    Basic Shiny app for switching between colour palettes

--- R/colours/app.R
+++ R/colours/app.R
@@ -1,3 +1,4 @@
+library(shiny) # Web Application Framework for R
 library(dplyr) # A Grammar of Data Manipulation
 library(janitor) # Simple Tools for Examining and Cleaning Dirty Data
 library(lubridate) # Make Dealing with Dates a Little Easier
@@ -20,7 +21,7 @@ df <- tribble(
   1, 1.
 )
 
-params <- tribble(
+layout <- tribble(
   ~xmin_coordinates_block, ~xmax_coordinates_block, ~x_coordinates_text, ~colour,   ~block_text,                       ~primary,
   0,                       0.17,                     0.075,             "#007CB9",  "Shore\nCalm, sanctuary",           TRUE,
   0.2075,                  0.3775,                   0.295,             "#00304B",  "Ocean\nExploration, frontier",     TRUE,
@@ -35,20 +36,33 @@ params <- tribble(
 img <- png::readPNG(here::here("R/colours/aklc.png"))
 g <- grid::rasterGrob(img, width = .3, interpolate=TRUE)
 
-ggplot(df) +
-  annotation_custom(g, xmin=0.21, xmax=0.81, ymin=-0.1, ymax=0.05) +
-  geom_text(aes(x = .175, y = 0.95, label = "Primary colour palette"), size = 6, fontface = "bold") +
-  pmap(list(params$xmin_coordinates_block[1:3], params$xmax_coordinates_block[1:3], params$primary[1:3], params$colour[1:3]), colour_block) +
-  pmap(list(params$x_coordinates_text[1:3], params$primary[1:3], params$block_text[1:3]), text_block) +
-  geom_text(aes(x = .2, y = 0.43, label = "Secondary colour palette"), size = 6, fontface = "bold") +
-  pmap(list(params$xmin_coordinates_block[4:8], params$xmax_coordinates_block[4:8], params$primary[4:8], params$colour[4:8]), colour_block) +
-  pmap(list(x_coordinates_text, rep(FALSE, 5), block_text[4:8]), text_block) +
-  scale_x_continuous(limits = c(0,1)) +
-  scale_y_continuous(limits = c(-0.05,1)) +
-  ggthemes::theme_fivethirtyeight() +
-  theme(
-    axis.text = element_blank(),
-    panel.grid.major = element_blank(),
-    title = element_text(family = "Arial", size = 15)
-  ) +
-  labs(title = "Te Kunihera o Tāmaki Makaurau brand colours\nand their associations")
+ui <- fluidPage(
+  radioButtons("colour_level", label = "Primary or Secondary Colour Palette?", choices = c(TRUE, FALSE)),
+  plotOutput("plot", width = "816px", height = "785px")
+)
+
+server <- function(input, output, session) {
+  params <- reactive(layout %>% filter(primary == input$colour_level))
+  
+  output$plot <- renderPlot({
+    ggplot(df) +
+      annotation_custom(g, xmin=0.21, xmax=0.81, ymin=-0.1, ymax=0.05) +
+      {if(input$colour_level == TRUE) geom_text(aes(x = .175, y = 0.95, label = "Primary colour palette"), size = 6, fontface = "bold")} +
+      {if(input$colour_level == TRUE) pmap(list(params()$xmin_coordinates_block[1:3], params()$xmax_coordinates_block[1:3], params()$primary[1:3], params()$colour[1:3]), colour_block)} +
+      {if(input$colour_level == TRUE) pmap(list(params()$x_coordinates_text[1:3], params()$primary[1:3], params()$block_text[1:3]), text_block)} +
+      {if(input$colour_level == FALSE) geom_text(aes(x = .2, y = 0.43, label = "Secondary colour palette"), size = 6, fontface = "bold")} +
+      {if(input$colour_level == FALSE) pmap(list(params()$xmin_coordinates_block[1:5], params()$xmax_coordinates_block[1:5], params()$primary[1:5], params()$colour[1:5]), colour_block)} +
+      {if(input$colour_level == FALSE) pmap(list(params()$x_coordinates_text, params()$primary[1:5], params()$block_text[1:5]), text_block)} +
+      scale_x_continuous(limits = c(0,1)) +
+      scale_y_continuous(limits = c(-0.05,1)) +
+      ggthemes::theme_fivethirtyeight() +
+      theme(
+        axis.text = element_blank(),
+        panel.grid.major = element_blank(),
+        title = element_text(family = "Arial", size = 15)
+      ) +
+      labs(title = "Te Kunihera o Tāmaki Makaurau brand colours\nand their associations")
+  })
+}
+
+shinyApp(ui, server)
