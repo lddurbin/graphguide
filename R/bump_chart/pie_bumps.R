34b1926464d6ae1b7d67930a6c89e4603ea1482c
lddurbin <l.d.durbin@gmail.com>
2022-07-07 19:33:28 +1200

    Use pies instead of circles

--- R/bump_chart/pie_bumps.R
+++ R/bump_chart/pie_bumps.R
@@ -3,6 +3,7 @@ library(dplyr) # A Grammar of Data Manipulation
 library(ggplot2) # Create Elegant Data Visualisations Using the Grammar of Graphics
 library(RColorBrewer) # ColorBrewer Palettes
 library(ggtext) # Improved Text Rendering Support for 'ggplot2'
+library(ggimage) # Use Image in 'ggplot2'
 
 path <- here::here("R/bump_chart")
 
@@ -13,19 +14,21 @@ pies <- readRDS(fs::dir_ls(path, glob = "*.rds")) |>
   janitor::clean_names()
 
 ranked_districts <- pies |> 
+  filter(district %in% c("Auckland", "Hawke's Bay", "Bay of Plenty", "Christchurch", "Waikato")) |> 
   count(year, district, name = "value") |> 
   arrange(year, desc(value)) |> 
   with_groups(year, mutate, rank = row_number()) |> 
   mutate(
     line_colour = if_else(district == "Hawke's Bay", "orange", "grey"),
-    district_colour = if_else(district == "Hawke's Bay", "orange", "black")
-    )
-
+    district_colour = if_else(district == "Hawke's Bay", "orange", "black"),
+    image = paste0(path, "/pie-icon.png")
+    ) 
+  
 plot <- ggplot(ranked_districts, aes(x = year, y = rank, group = district, label = rank, colour = line_colour)) +
   geom_line(size = 1.5) +
-  geom_point(size = 9) +
-  geom_text(color = "white", fontface = "bold", size = 4) +
-  scale_y_reverse(limits=c(8,1)) +
+  geom_image(aes(image = image), size = 0.08) +
+  geom_text(aes(y = rank+0.05), color = "white", fontface = "bold", size = 4) +
+  scale_y_reverse(limits=c(5.1,1)) +
   scale_x_continuous(position = "top", limits = c(2010.5,2021), breaks = seq(2012,2022, by = 1)) +
   scale_color_manual(values = c("#BEBEBE", "orange")) +
   annotate("text", x = rep(2010.75, 10), y = 1:10, label = ranked_districts$district[1:10], hjust=0.5, colour = ranked_districts$district_colour[1:10], size = 5) +
@@ -43,7 +46,7 @@ plot <- ggplot(ranked_districts, aes(x = year, y = rank, group = district, label
   ) +
   labs(
     title = "The fall and rise of <span style='color:orange'><strong>Hawke's Bay</strong></span> pies",
-    subtitle = "After slipping down the rankings, bakeries in Hawke's Bay are once again churning out some of the finest pies in Aotearoa",
+    subtitle = "Although no longer the best outside of Auckland, bakeries in Hawke's Bay are still churning out some of the finest pies in Aotearoa",
     caption = "New Zealand's districts ranked by number of award-winning pies"
   )
 
