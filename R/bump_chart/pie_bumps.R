4bc8c90718fba5ec169fa0b31859ece637e8edb3
lddurbin <l.d.durbin@gmail.com>
2022-07-08 13:24:16 +1200

    Moved subtitle into title, tweaked x-axis label positions

--- R/bump_chart/pie_bumps.R
+++ R/bump_chart/pie_bumps.R
@@ -25,29 +25,29 @@ ranked_districts <- pies |>
     ) 
   
 plot <- ggplot(ranked_districts, aes(x = year, y = rank, group = district, label = rank, colour = line_colour)) +
-  geom_line(size = 1.5) +
+  geom_line(size = 1.2) +
   geom_image(aes(image = image), size = 0.08) +
   geom_text(aes(y = rank+0.05), color = "white", fontface = "bold", size = 4) +
-  scale_y_reverse(limits=c(5.1,1)) +
+  scale_y_reverse(limits=c(5.1,0.9)) +
   scale_x_continuous(position = "top", limits = c(2010.5,2021), breaks = seq(2012,2022, by = 1)) +
   scale_color_manual(values = c("#BEBEBE", "orange")) +
-  annotate("text", x = rep(2010.75, 10), y = 1:10, label = ranked_districts$district[1:10], hjust=0.5, colour = ranked_districts$district_colour[1:10], size = 5) +
+  annotate("text", x = rep(2010.75, 5), y = (1:5)+0.05, label = ranked_districts$district[1:5], hjust=0.5, colour = ranked_districts$district_colour[1:5], size = 7) +
   theme_minimal() +
   theme(
     legend.position = "none",
     panel.grid = element_blank(),
     axis.title = element_blank(),
     axis.text.y = element_blank(),
-    axis.text.x = element_text(size = 16),
+    axis.text.x = element_text(size = 18),
     plot.title.position = "plot",
-    plot.title = element_markdown(size=20, lineheight = 1.1),
+    plot.title = element_markdown(size=22, lineheight = 1.1),
     plot.subtitle = element_markdown(margin=margin(0,0,20,0), size = 14),
     plot.caption = element_markdown(size=10)
   ) +
   labs(
-    title = "The fall and rise of <span style='color:orange'><strong>Hawke's Bay</strong></span> pies",
-    subtitle = "Although no longer the best outside of Auckland, bakeries in Hawke's Bay are still churning out some of the finest pies in Aotearoa",
-    caption = "New Zealand's districts ranked by number of award-winning pies"
+    title = "The fall and rise of <span style='color:orange'>**Hawke's Bay**</span> pies: although no longer the best outside of Auckland, bakeries<br>in <span style='color:orange'>**Hawke's Bay**</span> are still churning out some of the finest pies in Aotearoa",
+    # subtitle = "Although no longer the best outside of Auckland, bakeries in Hawke's Bay are still churning out some of the finest pies in Aotearoa",
+    subtitle = "New Zealand's districts ranked by number of award-winning pies"
   )
 
 ggsave(filename = "pies_ranked.png", plot = plot, device = "png", path = path, bg = "white")
